a9e79583db9f078429cceac459ad9c92697e4d39
Phrancium <tianfrank@gmail.com>
2018-03-10 19:03:57 -0500

    spam tim with messages

--- HackTJ2018/app/src/main/java/com/google/hacktj2018/MainActivity.java
+++ HackTJ2018/app/src/main/java/com/google/hacktj2018/MainActivity.java
@@ -1,6 +1,7 @@
 package com.google.hacktj2018;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -8,7 +9,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.*;
 
 
 // public static final String INBOX = "content://sms/inbox";
@@ -18,11 +22,15 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button search;
+    private EditText edit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        search = (Button)findViewById(R.id.search);
+        edit = (EditText)findViewById(R.id.editText);
 
 
 
@@ -41,6 +49,20 @@ public class MainActivity extends AppCompatActivity {
         } else {
             // empty box, no SMS
         }
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage("+15715334077", null, edit.getText().toString(), null, null);
+//                Intent m = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:"
+//                        + "15715334077"));
+//                m.putExtra("sms_body", edit.getText());
+//                startActivity(m);
+//                Intent i = new Intent(MainActivity.this,ResultActivity.class);
+//                startActivity(i);
+            }
+        });
 
     }
 }
